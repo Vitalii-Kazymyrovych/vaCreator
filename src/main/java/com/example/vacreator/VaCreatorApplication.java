9220c59e58c894de3c6736ec8e50d4d6af187dee
Vitalii Kazymyrovych <vitalii.kazymyrovych@gmail.com>
2025-09-15 14:58:18 +0300

    initial

---
+++ src/main/java/com/example/vacreator/VaCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.vacreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaCreatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vacreator/VaCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vacreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaCreatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
